350be366defe9ad3e670000ea8149a88f7ffb676
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2025-10-31 21:38:42 +0800

    refactor(nodes/nextcloud/*): disable remote login

--- nodes/nextcloud/storage/system.nix
+++ nodes/nextcloud/storage/system.nix
@@ -11,14 +11,6 @@
       inherit (nodeConstants) cluster node;
     };
 
-    remote = {
-      enable = true;
-      hashedPassword = "$y$j9T$PcTf.v0YqjUalOHj7XGGg1$z9wLA3O/0KIWesEPDuxs3zd1/dJ/UfVCzM.0S8jmR.0";
-      authorizedKeys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMYndghkby7QFFZ8476PT9RM7D2z+f4YyY16pd2TyT4A starryreverie@starrynix-homelab"
-      ];
-    };
-
     states = {
       "minio".mountPoint = "/var/lib/minio";
     };
