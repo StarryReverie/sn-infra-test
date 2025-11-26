3fc3ec74a009e4c9aa721944d92a90a378ebe437
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2025-11-26 23:08:42 +0800

    fix(hosts/homelab): incorrect option key for SSH authorized keys

--- hosts/homelab/nixos/system.nix
+++ hosts/homelab/nixos/system.nix
@@ -12,7 +12,7 @@
   time.timeZone = "Asia/Shanghai";
   i18n.defaultLocale = "en_US.UTF-8";
 
-  users.users.root.openssh.authorizedKeys = [
+  users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBHhBWBm0pl855WnAlKB6567DR3fzAWPYAbYI4YxmYFu starryreverie@starrynix-workstation"
   ];
 
