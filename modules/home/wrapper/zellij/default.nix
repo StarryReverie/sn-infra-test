090c7aa05d3af724583cff690acb3a1fa97d13c5
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2025-11-26 15:12:31 +0800

    feat(modules/home/wrapper/zellij): add a attaching-to-session helper

--- modules/home/wrapper/zellij/default.nix
+++ modules/home/wrapper/zellij/default.nix
@@ -24,4 +24,14 @@ in
     zj = "zellij";
     zd = "zellij --layout development";
   };
+
+  settings.zsh.initContent = ''
+    # Zellij helpers
+    ## Attach to a session selected using fzf
+    function za() {
+        zellij delete-all-sessions --yes 2>&1 > /dev/null
+        selected=$(zellij list-sessions | awk --field-separator ' ' '{ print $1 }' | sort | fzf --select-1 --height=~10)
+        [ -n "$selected" ] && zellij attach "$selected"
+    }
+  '';
 }
