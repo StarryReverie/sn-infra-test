10a395dff4fbbbb1ce3ab6a3394aa355b9dfbbcb
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2025-12-16 00:04:56 +0800

    feat(modules/users/starryreverie/helix): set default Nix language settings

--- modules/users/starryreverie/helix/default.nix
+++ modules/users/starryreverie/helix/default.nix
@@ -7,9 +7,11 @@
 {
   users.users.starryreverie = {
     maid = {
-      packages = [ pkgs.helix ];
+      packages = with pkgs; [ helix ];
 
       file.xdg_config."helix/config.toml".source = ./config.toml;
+      file.xdg_config."helix/languages.toml".source = ./languages.toml;
+
       file.xdg_config."helix/themes/one-dark-transparent.toml".source =
         ./themes/one-dark-transparent.toml;
       file.xdg_config."helix/themes/tokyo-night-storm-transparent.toml".source =
