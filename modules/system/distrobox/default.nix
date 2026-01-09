3eb3dc07c289f8c9f429ba48245d65ed7ecd6e2e
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2026-01-09 13:34:37 +0800

    fix(modules/system/distrobox): hardcoded bin path to /nix/store

--- modules/system/distrobox/default.nix
+++ modules/system/distrobox/default.nix
@@ -6,10 +6,16 @@
 }:
 {
   environment.systemPackages = with pkgs; [
-    distrobox
+    (distrobox.overrideAttrs {
+      # Distrobox hardcodes binary paths pointing to /nix/store in its
+      # configuration. Once the store path of distrobox is changed, the box
+      # will break.
+      # See <https://github.com/89luca89/distrobox/pull/1959>.
+      patches = [ ./fix-bin-path.patch ];
+    })
   ];
 
   environment.etc."distrobox/distrobox.conf".text = ''
-    container_additional_volumes="/nix/store:/nix/store:ro /etc/profiles/per-user:/etc/profiles/per-user:ro /etc/static/profiles/per-user:/etc/static/profiles/per-user:ro"
+    container_additional_volumes="/nix/store:/nix/store:ro /etc/profiles/per-user:/etc/profiles/per-user:ro"
   '';
 }
