55258a984c1a5e575c14c360f297d4db2eaccefd
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2026-01-13 14:17:16 +0800

    refactor(modules/users/starryreverie/qt-theme): fully reproducible

--- modules/users/starryreverie/qt-theme/default.nix
+++ modules/users/starryreverie/qt-theme/default.nix
@@ -26,23 +26,27 @@
             [General]
             theme = Orchis-solidDark
           '';
+
+          "qt5ct/qt5ct.conf".source = pkgs.replaceVars ./qt5ct.conf {
+            colorSchemePath = pkgs.libsForQt5.qt5ct + /share/qt5ct/colors/darker.conf;
+            iconTheme = "Reversal-dark";
+            style = "kvantum-dark";
+            font = "Open Sans";
+          };
+
+          "qt6ct/qt6ct.conf".source = pkgs.replaceVars ./qt6ct.conf {
+            colorSchemePath = pkgs.kdePackages.qt6ct + /share/qt6ct/colors/darker.conf;
+            iconTheme = "Reversal-dark";
+            style = "kvantum-dark";
+            font = "Open Sans";
+          };
         };
     };
 
     custom.environment = {
       sessionVariables = {
         QT_QPA_PLATFORMTHEME = "qt6ct:qt5ct";
-        QT_STYLE_OVERRIDE = "kvantum-dark";
       };
-    };
-  };
-
-  preservation.preserveAt."/nix/persistence" = {
-    users.starryreverie = {
-      directories = [
-        ".config/qt5ct"
-        ".config/qt6ct"
-      ];
     };
   };
 }
