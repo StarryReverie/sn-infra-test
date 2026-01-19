232a04d5708aa914c9e090b46b3aac36dcd1fe1d
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2026-01-19 23:21:24 +0800

    feat(i/h/topological): add SSH public keys

--- inventory/hosts/topological/users/starryreverie/top-level.nix
+++ inventory/hosts/topological/users/starryreverie/top-level.nix
@@ -51,6 +51,7 @@ in
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHQrkIsLMV70klKFtQY8JK5QgXKGyTpZcIaLarXG5dBv"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBHhBWBm0pl855WnAlKB6567DR3fzAWPYAbYI4YxmYFu starryreverie@superposition"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJFYQFEqRJVSW3kBXGRJ5QrgDYIPYl5mANwsXMmy26Ak starryreverie@DESKTOP-Q1328MM"
     ];
   };
 
