40c329e094d79e4e34d9b2ef89355fb4dc61c5fb
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2026-02-21 11:29:42 +0800

    feat(i/h/topological): disable seldom used services

--- inventory/hosts/topological/system/service.nix
+++ inventory/hosts/topological/system/service.nix
@@ -13,10 +13,7 @@
         deployment = {
           inherit (inputs.self) nodeConfigurations;
           enabledClusters = [
-            "jellyfin"
             "nextcloud"
-            "searxng"
-            "jupyter"
           ];
         };
 
@@ -29,31 +26,10 @@
           forwardPorts = [
             {
               protocol = "tcp";
-              sourcePort = 8096;
-              toCluster = "jellyfin";
-              toNode = "main";
-              destinationPort = 8096;
-            }
-            {
-              protocol = "tcp";
               sourcePort = 8081;
               toCluster = "nextcloud";
               toNode = "main";
               destinationPort = 80;
-            }
-            {
-              protocol = "tcp";
-              sourcePort = 8248;
-              toCluster = "searxng";
-              toNode = "main";
-              destinationPort = 8248;
-            }
-            {
-              protocol = "tcp";
-              sourcePort = 8799;
-              toCluster = "jupyter";
-              toNode = "main";
-              destinationPort = 8799;
             }
           ];
         };
